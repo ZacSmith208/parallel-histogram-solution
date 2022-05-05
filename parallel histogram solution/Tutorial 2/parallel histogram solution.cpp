76ce4f65048ac6a6ca4e2edbb74c1999ec55ff22
Zac <zacgordonsmith@gmail.com>
2022-05-05 14:12:26 +0100

    commit 1!

--- parallel histogram solution/Tutorial 2/parallel histogram solution.cpp
+++ parallel histogram solution/Tutorial 2/parallel histogram solution.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <CImg.h>
 
 using namespace cimg_library;
 static const int HIST_BINS = 256;
@@ -21,18 +21,66 @@ void print_help() {
 
 int main(int argc, char **argv) {
 	
-	//int platform_id = 0;
-	//int device_id = 0;
-	int *inputIm = NULL;
-	int* outputHist = NULL;
-	int imageRows;
-	int imageCols; 
-	inputIm = readBm
-	
+	int platform_id = 0;
+	int device_id = 0;
+	string image_filename = "test.pgm";
 
-	
-	
+	for (int i = 1; i < argc; i++) {
+		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
+		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
+		else if (strcmp(argv[i], "-l") == 0) { std::cout << ListPlatformsDevices() << std::endl; }
+		else if ((strcmp(argv[i], "-f") == 0) && (i < (argc - 1))) { image_filename = argv[++i]; }
+		else if (strcmp(argv[i], "-h") == 0) { print_help(); return 0; }
+	}
+
+	cimg::exception_mode(0);
+	try {
+		CImg<unsigned char> image_input(image_filename.c_str());
+		CImgDisplay disp_input(image_input, "input");
+
+		cl::Context context = GetContext(platform_id, device_id);
+		std::cout << "Runinng on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << std::endl;
+		cl::CommandQueue queue(context);
+		cl::Program::Sources sources;
+		AddSources(sources, "kernels/my_kernels.cl");
+		cl::Program program(context, sources);
 		
+		try {
+			program.build();
+		}
+		catch (const cl::Error& err) {
+			std::cout << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			std::cout << "Build Options:\t" << program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			std::cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			throw err;
+		}
+		cl::Buffer dev_image_input(context, CL_MEM_READ_ONLY, image_input.size());
+		cl::Buffer dev_image_output(context, CL_MEM_READ_WRITE, image_input.size());
+		queue.enqueueWriteBuffer(dev_image_input, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);
+		cl::Kernel kernel_1 = cl::Kernel(program, "hist_simple");
+		kernel_1.setArg(0, dev_image_input);
+		
+
+
+		
+		
+		vector<unsigned char> output_buffer(image_input.size());
+		queue.enqueueReadBuffer(dev_image_output, CL_TRUE, 0, output_buffer.size(), &output_buffer.data()[0]);
+		CImg<unsigned char> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
+		CImgDisplay disp_output(output_image, "output");
+		while (!disp_input.is_closed() && !disp_output.is_closed()
+			&& !disp_input.is_keyESC() && !disp_output.is_keyESC()) {
+			disp_input.wait(1);
+			disp_output.wait(1);
+		}
+	}
+	catch (const cl::Error& err) {
+		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
+	}
+	catch (CImgException& err) {
+		std::cerr << "ERROR: " << err.what() << std::endl;
+	}
+	
 
 	return 0;
 }
